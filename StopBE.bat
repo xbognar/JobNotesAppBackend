062e3c92ff46ea7aed91fef35c18754466d34bb2
Mate Bognar <x.bognar@stuba.sk>
2024-09-15 21:32:55 +0200

    Error fix

--- StopBE.bat
+++ StopBE.bat
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 @echo off
 
 REM Navigate to project directory
@@ -13,19 +12,3 @@ echo Stopping Docker Desktop...
 taskkill /F /IM "Docker Desktop.exe" >nul 2>&1
 
 echo Application has been successfully stopped.
-=======
-@echo off
-
-REM Navigate to project directory
-cd /d "path\to\your\project"
-
-REM Stop Docker services
-echo Stopping Docker services...
-docker-compose down
-
-REM Close Docker Desktop
-echo Stopping Docker Desktop...
-taskkill /F /IM "Docker Desktop.exe" >nul 2>&1
-
-echo Application has been successfully stopped.
->>>>>>> 68c53ca35de418c32724f1b55450d9d31fe874a7
